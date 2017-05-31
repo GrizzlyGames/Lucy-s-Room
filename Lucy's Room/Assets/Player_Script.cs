2a08a7898aaefbd25f4388307cccc8a66d85cf66
GrizzlyGamesCo <info@grizzlygames.co>
2017-05-30 23:11:20 -0400

    Updates

---
+++ Lucy's Room/Assets/Player_Script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Player_Script : MonoBehaviour
+{
+    public Text subtitle_Text;
+    private RaycastHit rayHit;
+    private Ray ray;
+
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // Identifies interactable objects
+        if (Physics.Raycast(ray, out rayHit, 3, 1 << 8))
+        {
+            subtitle_Text.text = "Debug: " + rayHit.collider.gameObject.name;
+        }
+        else
+            subtitle_Text.text = "";
+    }
+}
